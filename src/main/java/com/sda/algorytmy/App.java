c2b0de2ebfe6b0895dfca8b74d830268e3d0535d
Jacek.Woj <jacek.wojtuszek@gmail.com>
2019-05-26 15:55:13 +0100

    Sacramento

--- src/main/java/com/sda/algorytmy/App.java
+++ src/main/java/com/sda/algorytmy/App.java
@@ -1,13 +1,28 @@
 package com.sda.algorytmy;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class App {
+
+    public static Path file = Paths.get("real_estate.csv");
+
+    public static void main(String[] args) {
+        try {
+            List<String> lines = Files.readAllLines(file);
+            System.out.println(lines);
+
+            String[] l = lines.get(1).split(",");
+            System.out.println(2);
+
+            for (String line: lines){
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
